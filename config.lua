da0d02809e6c7935161b4031aa7f34ea2a353ee1
|Venom| <73774307+VenomGameexe@users.noreply.github.com>
2021-01-04 02:16:14 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,29 @@
+--    _   _ _____   _____ 
+--   | \ | |  __ \ / ____|
+--   |  \| | |__) | |     
+--   | . ` |  ___/| |     
+--   | |\  | |    | |____ 
+--   |_| \_|_|     \_____|
+--                        
+--                       
+
+Config = {}
+
+Config.VehicleNpc = 0.0 --SetVehicleDensityMultiplierThisFrame
+Config.Npc = 0.0 --SetPedDensityMultiplierThisFrame
+Config.VeicoliRandom = 0.0 --SetRandomVehicleDensityMultiplierThisFrame
+Config.VeicoliParked = 0.0 --SetParkedVehicleDensityMultiplierThisFrame
+Config.Scenario = 0.0 --SetScenarioPedDensityMultiplierThisFrame
+Config.GarbageTrucks = false --SetGarbageTrucks
+Config.Police = false --SetCreateRandomCops
+Config.PoliceScenarios = false --SetCreateRandomCopsOnScenarios
+Config.PoliceNotOnScenarios =  false --SetCreateRandomCopsNotOnScenarios
+
+--     _____ _____  ______       _______ ______ _____    ______     __  ___      __                        _   _  _    ___ ____ ______ ___  
+--    / ____|  __ \|  ____|   /\|__   __|  ____|  __ \  |  _ \ \   / / | \ \    / /                       | |_| || |_ / _ \___ \____  / _ \ 
+--   | |    | |__) | |__     /  \  | |  | |__  | |  | | | |_) \ \_/ /  | |\ \  / /__ _ __   ___  _ __ ___ | |_  __  _| (_) |__) |  / / (_) |
+--   | |    |  _  /|  __|   / /\ \ | |  |  __| | |  | | |  _ < \   /   | | \ \/ / _ \ '_ \ / _ \| '_ ` _ \| |_| || |_ \__, |__ <  / / > _ < 
+--   | |____| | \ \| |____ / ____ \| |  | |____| |__| | | |_) | | |    | |  \  /  __/ | | | (_) | | | | | | |_  __  _|  / /___) |/ / | (_) |
+--    \_____|_|  \_\______/_/    \_\_|  |______|_____/  |____/  |_|    | |   \/ \___|_| |_|\___/|_| |_| |_| | |_||_|   /_/|____//_/   \___/ 
+--                                                                     | |                                | |                               
+--                                                                     |_|                                |_|                               
---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author '|Venom|#9378'
+description 'NPC'
+version 'V1.0'
+
+
+client_scripts {
+    'config.lua',
+    'codice/npc_main.lua'
+}
+server_script 'Server/npc_server.lua'
