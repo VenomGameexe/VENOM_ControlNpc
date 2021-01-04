ea88ce4a38f320af6d7c68725a2ede711df89e19
|Venom| <73774307+VenomGameexe@users.noreply.github.com>
2021-01-04 02:25:46 +0100

    Create npc_server.lua

---
+++ Server/npc_server.lua
@@ -0,0 +1,8 @@
+Citizen.CreateThread(function()
+	Citizen.Wait(5000)
+	local ver = "1.0"
+	local prefix = "^0[^1|Venom|#9378^0]"
+	local info = "^0[^4INFO^0]"
+        print("-----------------------------------------------------------")
+	print(prefix..  info.. " ^2CONTROLNPC WORKING ^7V. "..ver)
+end)
